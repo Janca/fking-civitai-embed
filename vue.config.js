5d874c3dff5faf28948983138420ef302b31e4ba
fking <janca@fking.gay>
2023-08-12 07:08:44 -0500

    ghpages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-    publicPath: process.env.NODE_ENV === 'production' ? "/fking-civitai-embed/" : "/",
+    //publicPath: process.env.NODE_ENV === 'production' ? "/fking-civitai-embed/" : "/",
     transpileDependencies: true,
     outputDir: 'docs'
 })
