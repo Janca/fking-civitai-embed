6fadf501b2b443b9c54da7a74e501ae4f7fba405
fking <janca@fking.gay>
2023-08-12 07:04:09 -0500

    trying docs for ghpages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
     publicPath: process.env.NODE_ENV === 'production' ? "/fking-civitai-embed/" : "/",
-    transpileDependencies: true
+    transpileDependencies: true,
+    outputDir: 'docs'
 })
